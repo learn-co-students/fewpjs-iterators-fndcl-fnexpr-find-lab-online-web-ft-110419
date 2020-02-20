13249e506a75fb01b0e69d9e8c17978137ef2d23
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-02-20 11:20:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return "hi";
+}
+function superbowlWin(record) {
+  let result = record.find(record => record.result === "W");
+  return !!result ? result.year : undefined;
 }
