add03f6c3c79f7d360e3274a98aab98986bf830e
jessesbyers <jesse.s.byers@gmail.com>
2020-02-27 15:31:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+  let win = array.find(function(y) { return y.result === "W"})
+  if (win) {
+    return win.year
+  }
 }
