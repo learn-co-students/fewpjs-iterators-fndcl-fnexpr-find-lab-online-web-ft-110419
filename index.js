804f989646e6b057055429bfa33d8c2ca67b13b6
LucyWeltner <lucyiweltner@gmail.com>
2020-02-28 13:05:31 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+	let game = array.find(game => game.result === "W")
+	if (game) {
+		return game.year
+	}
+	else {
+		return undefined
+	}
+}
