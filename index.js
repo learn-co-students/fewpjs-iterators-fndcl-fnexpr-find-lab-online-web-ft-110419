053e0c67daddf7d519889c0746ccca30dd911e1b
Gilblasse <gilblasse@gmail.com>
2020-02-27 19:12:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return "hi";
+}
+
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" }
+];
+
+function superbowlWin(arr = []) {
+  let winingResult = arr.find(obj => obj.result == "W");
+  if (winingResult) {
+    return winingResult.year;
+  }
 }
