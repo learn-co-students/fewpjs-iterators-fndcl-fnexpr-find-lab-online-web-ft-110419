fdcffd49a818f8dd7ae27c7cae56881efb7e54a0
Ethan <ethan_gustafson@outlook.com>
2020-02-28 22:11:50 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let recordYear = record.find(function(object) {
+     if (object.result === "W"){
+     return object
+     }
+  })
+
+  if (!!recordYear) {
+    return recordYear.year
+  } 
+  else {
+    return recordYear
+  }
+
+  // if (record.find(object => object.year === recordYear)){
+  //   return recordYear
+  // }
+  // else {
+  //   return undefined
+  // }
+}
