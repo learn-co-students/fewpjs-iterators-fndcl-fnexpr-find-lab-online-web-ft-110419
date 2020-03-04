6dd4f62df732ba2239cc07ce959819b679d2231d
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-04 07:59:05 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let winner = array.find(findWin)
+ return winner ? winner.year : undefined
+}
+
+function findWin(obj) {
+  return obj.result === 'W'
+}
