58157145c769d8b7490db5cdf8e7304ef69dd0cb
Dominique Skaf <doskaf@yahoo.com>
2020-03-05 17:05:31 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objects) {
+  let win = objects.find(function(e) { return e.result === "W"})
+  if (win) {
+    return win.year
+  }
+}
